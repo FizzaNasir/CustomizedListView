ca01c09ae2262e27c5237697618fc864e8888ba2
Fizza <fizzanasir976@gmail.com>
2022-05-28 10:25:33 +0500

    basic list view and array adaptor

--- ListView/app/src/main/java/com/fizzanasir/listview/MainActivity.java
+++ ListView/app/src/main/java/com/fizzanasir/listview/MainActivity.java
@@ -3,6 +3,10 @@ package com.fizzanasir.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listview=findViewById(R.id.fruits);
+        ArrayList<String> fruitList = new ArrayList<String>();
+        fruitList.add("Mango");
+        fruitList.add("Apple");
+        fruitList.add("Banana");
+        fruitList.add("Peach");
+        fruitList.add("Apricot");
+        fruitList.add("Cherry");
+
+        ArrayAdapter<String> adaptor=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, fruitList);
+        listview.setAdapter(adaptor);
+
     }
 }
